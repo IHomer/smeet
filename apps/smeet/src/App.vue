641e416eab12f203f158116084131ab700aa0033
Bart in t Veld <bart.int.007@gmail.com>
2023-01-30 15:03:27 +0100

    Installed appollo

--- apps/smeet/src/App.vue
+++ apps/smeet/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  {{ result }}
 </template>
 
-<script lang="ts">
+<script lang="ts" set>
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
+</script>
+
+<script lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+const { result } = useQuery(gql`
+      query getCountry {  
+        country(code: "NL") {
+          name
+          native
+          capital
+          emoji
+          currency
+          languages {
+            code
+            name
+          }
+        }
+      }
+    `)
 </script>
--- apps/smeet/src/main.ts
+++ apps/smeet/src/main.ts
@@ -1,6 +1,33 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import App from "./App.vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
 import "./index.css";
 
-createApp(App).mount("#app");
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "https://countries.trevorblades.com/",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+}).mount("#app");
