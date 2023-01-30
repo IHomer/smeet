0226f2a6c4da8e114a0c48c7d6f91605205a04cc
Bart in t Veld <bart.int.007@gmail.com>
2023-01-30 14:39:53 +0100

    Added tailwind

--- apps/smeet/src/App.vue
+++ apps/smeet/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
@@ -14,14 +13,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
