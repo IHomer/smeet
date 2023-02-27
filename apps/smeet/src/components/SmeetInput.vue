c8069466382e6389ecc52be782e84d6a60cacfb9
paulhobbel <itsdizzy40@gmail.com>
2023-02-27 10:17:36 +0100

    easier to send chat messages now :P

--- apps/smeet/src/components/SmeetInput.vue
+++ apps/smeet/src/components/SmeetInput.vue
@@ -5,17 +5,19 @@
     </label>
     <input
       class="px-3 py-2 border rounded border-gray-300 bg-zinc-600 text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      ref="inputRef"
       v-model="value"
       :title="label"
       :name="name"
       :type="type"
       :disabled="disabled"
+      :autofocus="autofocus"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps([
   'modelValue',
@@ -24,15 +26,24 @@ const props = defineProps([
   'type',
   'error',
   'disabled',
+  'autofocus'
 ]);
 const emit = defineEmits(['update:modelValue']);
+
+const inputRef = ref<HTMLInputElement>();
 
 const value = computed({
   get() {
     return props.modelValue;
   },
-  set(value: any) {
+  set(value: string) {
     emit('update:modelValue', value);
   },
 });
+
+const focus = () => {
+  inputRef.value?.focus();
+}
+
+defineExpose({ focus });
 </script>
--- apps/smeet/src/pages/ChatPage.vue
+++ apps/smeet/src/pages/ChatPage.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="flex flex-col max-h-screen overflow-hidden">
-    <div class="flex-1 overflow-scroll" id="messages">
+    <div class="flex-1 overflow-scroll" ref="messageContainerRef">
       <ChatMessage
         v-for="message in messages"
         :key="message.id"
         :message="message"
       />
     </div>
-    <div class="flex-initial h-16 p-2 border-t border-gray-500 flex gap-2">
+    <form class="flex-initial h-16 p-2 border-t border-gray-500 flex gap-2" @submit.prevent="onSubmit">
       <div class="flex-1">
         <SmeetInput
+          ref="inputRef"
           name="message"
+          autofocus
           v-model="message"
           :disabled="isLoadingCreate"
+          :error="errors.message"
         />
       </div>
       <SmeetButton
-        :disabled="!canSendMessage || isLoadingCreate"
-        @click="doSendMessage"
+        type="submit"
+        :disabled="!meta.valid || isLoadingCreate"
       >
         <font-awesome-icon :icon="['fas', 'paper-plane']" />
       </SmeetButton>
-    </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { watch, ref, computed } from 'vue';
+import { ref, watch } from 'vue';
+
+import { useField, useForm } from 'vee-validate';
+import { toFormValidator } from '@vee-validate/zod';
+import z from 'zod';
+
 import ChatMessage from '../components/ChatMessage.vue';
 import SmeetButton from '../components/SmeetButton.vue';
 import SmeetInput from '../components/SmeetInput.vue';
@@ -34,25 +42,32 @@ import { useCreateMessage } from '../queries/message/createMessage';
 import { useListMessages } from '../queries/message/listMessages';
 import { useUserStore } from '../store/user.store';
 
-const message = ref('');
-
 const { name } = useUserStore();
 const { mutate, loading: isLoadingCreate } = useCreateMessage();
 const { messages, loading: isLoadingList } = useListMessages();
 
-const canSendMessage = computed(() => message.value !== '');
+const messageContainerRef = ref<HTMLDivElement>();
+const inputRef = ref<typeof SmeetInput>();
 
-watch(messages, () => {
-  const messagesEl = document.getElementById('messages');
-  if (messagesEl) {
-    messagesEl.scrollTop = messagesEl.scrollHeight;
-  }
+const formSchema = z.object({
+  message: z.string().min(3, { message: 'Too short' }),
+});
+const validationSchema = toFormValidator(formSchema);
+
+const { handleSubmit, errors, meta } = useForm({
+  validationSchema,
+});
+const { value: message } = useField('message');
+
+const onSubmit = handleSubmit(async ({ message }, { resetForm }) => {
+  await mutate({ message: { user: name, message: message, bot: false } });
+  resetForm();
+  inputRef.value?.focus();
 });
 
-function doSendMessage() {
-  if (canSendMessage.value) {
-    mutate({ message: { user: name, message: message.value, bot: false } });
-    message.value = '';
+watch(messages, () => {
+  if (messageContainerRef.value) {
+    messageContainerRef.value.scrollTop = messageContainerRef.value.scrollHeight;
   }
-}
+});
 </script>
