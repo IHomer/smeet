cab4c37971e6bbd474a56cabb85f24ccd4bdcdcf
paulhobbel <itsdizzy40@gmail.com>
2023-02-28 10:54:04 +0100

    Add fork me ribbon

--- apps/smeet/src/App.vue
+++ apps/smeet/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <RegisterPage v-if="name === ''" />
   <ChatPage v-else />
+  <ForkMe />
 </template>
 
 <script lang="ts" setup>
 import { useUserStore } from './store/user.store';
 import RegisterPage from './pages/RegisterPage.vue';
 import ChatPage from './pages/ChatPage.vue';
+import ForkMe from './components/ForkMe.vue';
 import { storeToRefs } from 'pinia';
 
 const { name } = storeToRefs(useUserStore());
